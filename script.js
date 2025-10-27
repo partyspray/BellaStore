5d4522c2b24bd014b7991bb5e91ceef7fcbaed33
partyspray <43161177+partyspray@users.noreply.github.com>
2025-10-27 07:19:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+const buyButton = document.querySelector('.buy-button');
+
+buyButton.addEventListener('click', () => {
+  alert('تم إضافة المنتج إلى سلة الشراء');
+  // أضف كود لإضافة المنتج إلى سلة الشراء هنا
+});
